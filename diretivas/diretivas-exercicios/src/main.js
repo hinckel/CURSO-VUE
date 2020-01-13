5a72a9e8e2d6c35d66ce31f9f0336b51fc3720ff
Hinckel <hinckel.luiz@gmail.com>
2020-01-13 06:48:06 -0300

    Vue Directives

---
+++ diretivas/diretivas-exercicios/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.directive('destaque', {
+	bind(el, binding/*, vnode*/) {
+		//el.style.backgroundColor = 'lightgreen'
+		//el.style.backgroundColor = binding.value
+		
+		if (binding.arg === 'fundo') {
+			el.style.backgroundColor = binding.value
+		} else {
+			el.style.color = binding.value
+		}
+	}
+})
+
+new Vue({
+	render: h => h(App),
+}).$mount('#app')
