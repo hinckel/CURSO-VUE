1c9b23230e1d8b60f6edf6cbe85cfecf8c6ef32e
Hinckel <hinckel.luiz@gmail.com>
2020-02-25 10:33:36 -0300

    routes basic

---
+++ rotas-exercicios/src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Inicio from './components/Inicio'
+import Usuario from './components/usuario/Usuario'
+
+Vue.use(Router)
+
+export default new Router({
+  //mode: 'hash',
+  mode: 'history',
+  routes: [{
+    path: '/',
+    component: Inicio
+  }, {
+    path: '/usuario',
+    component: Usuario
+  }]
+})
