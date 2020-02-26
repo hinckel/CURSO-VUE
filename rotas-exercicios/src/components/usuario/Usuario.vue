65dafa883354d9047cb7aa1776737efdd19be7d9
Hinckel <hinckel.luiz@gmail.com>
2020-02-26 07:55:06 -0300

    routes, route chieldren and path props

--- rotas-exercicios/src/components/usuario/Usuario.vue
+++ rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="usuario">
     <h2>Usuários</h2>
+    <hr>
+    <router-view />
+    <button sucesso @click="goToBegin()">Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
-  
+  methods: {
+    goToBegin() {
+      this.$router.push({ path: '/' })
+    }
+  }
 }
 </script>
 
---
+++ rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="usuario-editar">
+    <h3>Usuário Editar</h3>
+    <p><strong>Código: </strong> {{ id }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+}
+</script>
+
+<style>
+  
+</style>
